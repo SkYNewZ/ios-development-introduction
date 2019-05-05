487cb7182bb2ae55c3e2117ac1c8d9a15c3578ed
Quentin Lemaire <quentin.lemaire10@free.fr>
2019-05-05 23:10:14 +0200

    Finish course

--- Teki/ViewController.swift
+++ Teki/ViewController.swift
@@ -9,7 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let celebrities = [
+        "le Steve Jobs",
+        "le Zinedine Zidane",
+        "la Madonna",
+        "le Karl Lagarfeld",
+        "la Scarlett Johansson"
+    ]
+    let activities = [
+        "du dancefloor",
+        "du barbecue",
+        "de la surprise ratée",
+        "des blagues lourdes",
+        "de la raclette party"
+    ]
+    
+    @IBOutlet weak var quoteLabel: UILabel!
+    
+    @IBAction func changeQuote() {
+        quoteLabel.text = "coucou"
+        let randomIndexCelebrity = Int(arc4random_uniform(UInt32(celebrities.count)))
+        let randomIndexActivity = Int(arc4random_uniform(UInt32(activities.count)))
+        
+        quoteLabel.text = "Tu es " + celebrities[randomIndexCelebrity] + " " + activities[randomIndexActivity] + "!"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
